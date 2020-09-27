a5f693e7349f0d6116bd569278f1d4acb0dee16b
Daniel <daniel.grondal@gmail.com>
2020-09-27 12:22:26 +0200

    Using api-key to create new gamelayer. Runnable game now.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ api_key = "fdeefade-dbbe-4b3f-8374-7563da254238"   # TODO: Your api key here
 # The different map names can be found on considition.com/rules
 map_name = "training1"  # TODO: You map choice here. If left empty, the map "training1" will be selected.
 
-game_layer: GameLayer = None
+game_layer: GameLayer = GameLayer(api_key)
 
 
 def main():
